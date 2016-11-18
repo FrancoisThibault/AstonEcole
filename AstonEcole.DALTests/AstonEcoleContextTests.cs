c2abbc7fc54568300dcced7319fd5af0832b537c
François Thibault <ftibo@free.fr>
2016-11-18 13:50:52 +0100

    ajout projet test

---
+++ AstonEcole.DALTests/AstonEcoleContextTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AstonEcole.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstonEcole.DAL.Tests
+{
+    [TestClass()]
+    public class AstonEcoleContextTests
+    {
+        [TestMethod()]
+        public void AstonEcoleContextTest()
+        {
+            AstonEcoleContext ctx = new AstonEcoleContext();
+            var x = ctx.Students.Select(s => new { Student = s, NbCours = s.Courses.Count() });
+           // Assert.Fail();
+        }
+    }
+}
